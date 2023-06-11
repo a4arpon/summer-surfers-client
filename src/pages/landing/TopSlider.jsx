dbf5848c36209def1a46b2a9d7aee63e2cd5d758
Mr Wayne <a4arpon@gmail.com>
2023-06-11 22:52:52 +0600

    fix: slider enhanced

--- src/pages/landing/TopSlider.jsx
+++ src/pages/landing/TopSlider.jsx
@@ -8,33 +8,33 @@ const TopSlider = () => {
     {
       id: 1,
       title: 'Peoples hobby of keeping memories is very old.',
-      img: 'https://colorfully.eu/wp-content/uploads/2012/09/autumn-fall-photography-dry-leaves-canon-eos-30d-facebook-cover.jpg',
+      img: 'https://images.unsplash.com/photo-1580892047528-7dfd384dce65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&h=360&q=80',
       subTitle: '12 May 2023',
     },
     {
       id: 2,
       title:
         'At one time, people were doing photography only as a hobby, but with the change of era, there has been a change in peoples thinking.',
-      img: 'https://colorfully.eu/wp-content/uploads/2012/12/big-camera-girl.jpg',
+      img: 'https://images.unsplash.com/photo-1602445470458-d67e1c9ae852?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&h=360&q=80',
       subTitle: '12 May 2023',
     },
     {
       id: 3,
       title: 'explore the art of photography with your creativity',
-      img: 'https://digitaldefynd.com/wp-content/uploads/2018/07/best-photography-course-tutorial-class-certification-training-online.webp',
+      img: 'https://images.unsplash.com/photo-1431068799455-80bae0caf685?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&h=360&q=80',
       subTitle: '12 May 2023',
     },
     {
       id: 4,
       title: 'Now many people have chosen photography as a profession.',
-      img: 'https://images.shiksha.com/mediadata/images/articles/1439545106phpuJSsZN.jpeg',
+      img: 'https://images.unsplash.com/photo-1480365501497-199581be0e66?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&h=360&q=80',
       subTitle: '12 May 2023',
     },
     {
       id: 5,
       title:
         'So SummerSurfers is for those who want to learn photography as a hobby or as a profession',
-      img: 'https://resources.reed.co.uk/courses/coursemedia/365865/83808396-f20a-47d3-abe3-027284cedc82_cover.jpg',
+      img: 'https://images.unsplash.com/photo-1542038784456-1ea8e935640e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&h=360&q=80',
       subTitle: '12 May 2023',
     },
   ]
@@ -56,11 +56,10 @@ const TopSlider = () => {
             style={{ backgroundImage: `url(${item.img})` }}
             className="bg-no-repeat bg-cover bg-center lg:rounded-lg"
           >
-            <div className="w-full h-96 lg:rounded-lg bg-black bg-opacity-30 flex flex-col lg:flex-row p-5 text-white">
-              <div className="w-full text-3xl lg:text-5xl flex items-center bg-black bg-opacity-30 rounded-3xl rounded-bl-none p-5 text-center">
+            <div className="w-full h-96 lg:rounded-lg bg-black bg-opacity-30 flex flex-col lg:flex-row p-5 text-white items-center justify-center">
+              <div className="w-full text-3xl lg:text-7xl flex items-center bg-black bg-opacity-30 rounded-3xl p-5 text-center h-full justify-center">
                 <h1 style={{ fontFamily: 'Satisfy, cursive' }}>{item.title}</h1>
               </div>
-              <div className="w-full"></div>
             </div>
           </SwiperSlide>
         ))}
